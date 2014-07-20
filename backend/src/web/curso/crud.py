46d34134f9460279d7fdceaa32dd04caac404d72
jasielserra <jasiel_serra@hotmail.com>
2014-07-19 23:20:48 -0300

    Código referente ao Modulo Unittest Testbed e Classe GAETestCase Biblioteca de Mock
    Código referente aos templates
    Passagem de valores
    Impressão de valores
    Comandor for

---
+++ backend/src/web/curso/crud.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import, unicode_literals
+from zen import router
+from web.curso.rest import Curso
+
+def index(_write_tmpl):
+    query = Curso.query().order(-Curso.nome)
+    dct = {'lista_cursos':query.fetch()}
+     _write_tmpl('/templates/curso_listar.html', dct)
+
+def form(_write_tmpl):
+    path = router.to_path(salvar)
+    dct = {'salvar_curso':path}
+    _write_tmpl('/templates/curso_form.html', dct)
+
+def salvar(_handler,nome,preco):
+    preco = float(preco)
+    curso = Curso(nome=nome, preco=preco)
+    curso.put()
+    path = router.to_path(index)
+    _handler.redirect(path)
+
